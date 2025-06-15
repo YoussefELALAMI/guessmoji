c3c471162e29c326b396888a6da81f4f2b04d2cf
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-06-15 12:48:59 +0200

    Main Application

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 # app.py : Main application entry point
 
-from flask import Flask # type: ignore
+from flask import Flask
+from flask_cors import CORS
 from routes.game import game_bp
+from models.database import db
 
 app = Flask(__name__)
-app.register_blueprint(game_bp, url_prefix="/api")
+CORS(app)
+app.register_blueprint(game_bp)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guessmoji.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable SQLAlchemy tracking modifications to save memory
 
-if __name__ == "__main__":
+db.init_app(app)  # Initialize the SQLAlchemy instance with the Flask app
+
+if __name__ == "__main__": # => This block ensures the app runs only when executed directly, not imported
     app.run(debug=True)
