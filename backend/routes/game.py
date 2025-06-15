64b71e3b1e7568506b17e0faa034816e4e42630b
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-06-15 12:48:38 +0200

    API Routes Logic

--- backend/routes/game.py
+++ backend/routes/game.py
@@ -1,1 +1,19 @@
-# game.py : API routes logic
+# game.py : API routes logic, this file will contain the routes related to the game
+
+from flask import Blueprint, jsonify, request
+from models.database import Category, Emojis
+from models.database import db
+
+game_bp = Blueprint('game', __name__, url_prefix='/game')
+
+@game_bp.route("/categories", methods=["GET"])
+def get_categories():
+    
+    categories = Category.query.all()
+    categories = [{"id": category.id, "name": category.name} for category in categories]
+    return jsonify(categories), 200
+
+@game_bp.route("/quizzes", methods=["GET"])
+def get_quizzes():
+    quizzes = Emojis.query.all()
+    return jsonify([{"id": quiz.id, "emojis": quiz.emojis, "answer": quiz.answer, "category_id": quiz.category_id} for quiz in quizzes]), 200
