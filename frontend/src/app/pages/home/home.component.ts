9647eab039aa4911be91670245a17cf5f9fe2518
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-07-27 22:04:53 +0200

    Updating the Home view

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { ApiService } from '../../services/api.service';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -16,29 +16,32 @@ export class HomeComponent {
   private apiService = inject(ApiService);
 
   // Properties for the component
-  categories: any[] = [];
+  categories: String[] = [];
   other: boolean = false;
   quizParamsForm = new FormGroup({
-    category: new FormControl(''),
-    customCategory: new FormControl(''),
-    difficulty: new FormControl(''),
-    numberOfQuestions: new FormControl(5)
+    category: new FormControl('', [Validators.pattern('[a-zA-Z ]*')]),
+    customCategory: new FormControl('', [Validators.pattern('[a-zA-Z ]*')]),
+    difficulty: new FormControl('', [Validators.required]),
+    numberOfQuestions: new FormControl(5, [Validators.required, Validators.min(1), Validators.max(15)])
   });
 
   ngOnInit(): void {
-    this.apiService.getCategories().subscribe({
-      next: (data) => { 
-        this.categories = data;
-        console.log('Categories fetched:', this.categories);
-      },
-      error: (error) => {
-        console.error('Error fetching categories:', error);
-      }
-    });
+    // Any initialization logic can go here
+    this.categories = ['Food', 'Movies', 'Songs', 'Actors', 'Countries', 'History', 'F1 Drivers'];
   }
 
   startQuiz() {
     const params = this.quizParamsForm.value;
     console.log('Starting quiz with params:', params);
+    this.apiService.startQuiz(params).subscribe({
+      next: (data) => {
+        console.log('Quiz started successfully:', data);
+        // Handle successful quiz start, e.g., navigate to quiz page
+      },
+      error: (error) => {
+        console.error('Error starting quiz:', error);
+        // Handle error, e.g., show an error message
+      }
+    });
   }
 }
