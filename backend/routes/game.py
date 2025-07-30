a9ef0e03e89b7c866a34c74671e650d500dd8bdd
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-07-30 17:29:28 +0200

    Add Endpoint to get the Category's id

--- backend/routes/game.py
+++ backend/routes/game.py
@@ -8,15 +8,24 @@ game_bp = Blueprint('api', __name__, url_prefix='/api')
 
 @game_bp.route("/categories", methods=["GET"])
 def get_categories():
-    
     categories = Category.query.all()
     categories = [{"id": category.id, "name": category.name} for category in categories]
     return jsonify(categories), 200
 
-@game_bp.route("/quizzes", methods=["GET"])
-def get_quizzes():
-    quizzes = Emojis.query.all()
-    return jsonify([{"id": quiz.id, "emojis": quiz.emojis, "answer": quiz.answer, "category_id": quiz.category_id} for quiz in quizzes]), 200
+@game_bp.route("/categories/<string:category_name>", methods=["GET"])
+def get_category_id(category_name):
+    category = Category.query.filter_by(name=category_name).first()
+    if not category:
+        return jsonify({"error": "Category id not found"}), 400
+    return jsonify({"id": category.id}), 200
+
+@game_bp.route("/quiz/<string:id>/<int:length>", methods=["GET"])
+def get_quiz_by_id(id, length):
+    quiz = Emojis.query.filter_by(category_id=id).order_by(Emojis.id.desc()).limit(length).all()
+    if not quiz:
+        return jsonify({"error": "Quiz not found"}), 404
+    quiz = list(reversed(quiz))
+    return jsonify([{"question": question.emojis, "answer": question.answer} for question in quiz]), 200
 
 @game_bp.route("/start-quiz", methods=["POST"])
 def start_quiz():
@@ -27,12 +36,19 @@ def start_quiz():
     custom_category = data.get('customCategory')
     difficulty = data.get('difficulty')
     number_of_questions = data.get('numberOfQuestions', 5)
-    if (not category and not custom_category) or not difficulty or not isinstance(number_of_questions, int) or number_of_questions <= 0 or number_of_questions > 15:
+    if ((not category 
+         and not custom_category) 
+        or not difficulty 
+        or not isinstance(number_of_questions, int) 
+        or number_of_questions <= 0 
+        or number_of_questions > 15
+        ):
         return jsonify({"error": "Invalid parameters"}), 400
     emojis = generate_emoji(custom_category or category, difficulty, number_of_questions)
     if not emojis:
         return jsonify({"error": "Failed to generate emojis"}), 500
     save_quizzes_to_db(emojis, custom_category or category)
     print("Saved quizzes to DB!")
-    # Ensure a valid response is always returned
-    return jsonify({"message": "Quiz started", "category": category, "customCategory": custom_category, "difficulty": difficulty, "numberOfQuestions": number_of_questions}), 200
+    return jsonify({"category": category or custom_category,
+                           "length": number_of_questions,
+                             "questions": emojis}), 200
--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Question, QuizData } from './quiz.services';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +17,21 @@ export class ApiService {
     return this.http.get(`${this.apiUrl}/categories`);
   }
 
+  // GET request to obtain the id
+  getCategoryId(category: string): Observable<number> {
+    return this.http.get<{id: number}>(`${this.apiUrl}/categories/${category}`).pipe(
+      map(response => response.id)
+    );
+  }
+
+  // GET request to fetch quiz data by category and length
+  getQuizByCategory(categoryId: number, length: number): Observable<Question[]> {
+    return this.http.get<Question[]>(`${this.apiUrl}/quiz/${categoryId}/${length}`);
+  }
+
   // POST request to start a quiz with given parameters
-  startQuiz(params: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/start-quiz`, params);
+  startQuiz(params: any): Observable<QuizData> {
+    return this.http.post<QuizData>(`${this.apiUrl}/start-quiz`, params);
   }
 
 }
